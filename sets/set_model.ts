605fa88dea4adeac8fd6ab3014c5076b13d04ce8
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-10-04 20:28:24 -0300

    Aula 04/10/23

---
+++ sets/set_model.ts
@@ -0,0 +1,19 @@
+
+
+class Conjunto<T>{
+    elementos: T [];
+    inserir(elemento:T):boolean{
+        return false;
+    }
+    pertence(elemento: T): boolean{
+        return false;
+    };
+    uniao(conjunto_1: Conjunto<T>, conjunto_2: Conjunto<T>): Conjunto<T>{
+        
+        return {} as Conjunto<T>;
+    };
+    interseccao(conjunto_1: Conjunto<T>, conjunto_2: Conjunto<T>):Conjunto<T>{
+       return {} as Conjunto<T>; 
+    }; 
+}
+
